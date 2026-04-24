c3d499dbb88c3b997d69793d712215ea6a2bfae4
ASSOGBA Léonie <asgleonie@gmail.com>
2026-04-24 23:42:15 +0100

    Bloc 4 : persistance SQLite finalisee

---
+++ database/emprunt_dao.py
@@ -0,0 +1,60 @@
+from db_manager import DBManager
+
+
+class EmpruntDAO:
+    """Classe gerant les operations sur les emprunts en base de donnees."""
+    
+    def __init__(self):
+        """Iniatilise le DAO avec le gestionnaire de connexion."""
+        self.db =DBManager()
+        
+        
+     def ajouter_emprunteur(self, emprunteur):
+         """Ajoute un emprunteur dans la base de donnees."""
+         connexion = self.db.get_connexion()
+         curseur = connexion.cursor()
+         curseur.execute('''
+                         INSERT INTO emprunteurs (nom, prenom, email)
+                         Vlues(?,?,?)
+                         ''',(emprunteur.nom, emprunteur.prenom, emprunteur.email))
+         connexion.commit()
+         connexion.close()
+         print(f"Emprunteur  '{emprunteur.nom}' sauvegarde en base de donnees.")
+    
+    def ajouter_emprunt(self, emprunt):
+        connexion = self.db.get_connexion()
+        curseur =connexion.cursor()
+        curseur.execute('''
+                        INSERT INTO emprunts(id_media, id_emprunteur, date_emprunt,date_retour_prevue,rendu)
+                        VALUES(?,?,?,?,?)
+                        ''',(
+                        emprunt.media.id,
+                        emprunt.emprunteur.id,
+                        str(emprunt.date_emprunt),
+                        str(emprunt.date_retour_prevue)
+                        emprunt.rendu
+                        
+                        ))
+        connexion.commit()
+        connexion.close()
+        print(f"Emprunt sauvegarde en base de donnees.")
+        
+    def get_emprunts_en_cours(self):
+        """Recupere tous les emprunts en cours."""
+        connexion = self.db.get_connexion()
+        curseur = connexion.cursor()
+        curseur.execute("SELECT * FROM emprunts WHERE RENDU = 0")
+        emprunts = curseur.fetchall()
+        connexion.close()
+        return emprunts
+    def get_historique(self):
+        """Recupere tout l'historique des emprunts."""
+        conexion = self;db.get_connexion()
+        curseur = connexion.cursor()
+        curseur.execute("SELECT * FROM emprunts")
+        emprunts = curseur.fetchall()
+        connexion.close()
+        return emprunts
+        
+         
+                 ''')   
---
+++ database/media_dao.py
@@ -0,0 +1,59 @@
+from db_manager import DBManager
+
+
+class MediaDAO:
+    """Classe gerant les operations sur les medias en base de donnees."""
+    
+    def __init__(self):
+        """Initialise le DAO avec le gestionnaire de connexion."""
+        self.db = DBManager()
+    def ajouter_media(self, media):
+        """Ajoute un media dans la base de donnees."""
+        connexion = self.db.get_connexion()
+        curseur = connexion.cursor()
+        curseur.execute('''
+                        INSERT INTO medias(titre,genre,annee,type,disponible,auteur,isbn,artiste,nb_pistes,platforme,nb_joueurs)
+                        VALUES(?,?,?,?,?,?,?,?,?,?,?)
+                        ''',(
+                            media.titre,media.genre,media.annee,
+                            type(media).__name__, media.disponible,
+                            getattr(media, 'auteur', None),
+                            getattr(media, 'isbn', None),
+                            getattr(media, 'artiste' None),
+                            getattr(media, 'nb_pistes' None),
+                            getattr(media, 'platforme' None),
+                            getattr(media, 'nb_joueurs', None),            
+        ))
+        connexion.commit()
+        connexion.close()
+         print(f"Media '{media.titre}' sauvegarde en base de donnees.")
+         
+    def get_tous_les_medias(self):
+        """Recupere tous les medias de la base de donnees."""
+        connexion =self.db.get_connexion()
+        curseur = connexion.cursor()
+        curseur.execute("SELECT * FROM medias")
+        medias = curseur.fetchall()
+        connexion.close()
+        return medias
+    
+    def rechercher_par_titre(self, titre):
+        """Recherche des medias par titre."""
+        connexion = self.db.get_connexion()
+        curseur = connexion.cursor()
+        curseur.execute(
+            "SELECT * FROM medias WHERE titre LIKE ?", ('%' +titre+ '%',)
+        )        
+        resultats = curseur.fetchall()
+        connexion.close()
+        return resultats
+    
+    def supprimer_media(self,id_media):
+        """Supprimer un media de le base de donnees."""
+        connexion = self.db.get_connexion()
+        curseur = connexion.execute("DELETE FROM medias WHERE id = ?",(id_media,))
+        connexion.commit()
+        connexion.close()
+        print(f"Media {id_media} supprime de la base de donnees.")
+        
+    
